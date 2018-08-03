820fcb46068aa1d3a0f47a909cd324e7e52be0b6
nbcallah <nbcallah@indiana.edu>
2018-08-02 21:22:17 -0400

    Fixed some typos in setup.h

--- setup.h
+++ setup.h
@@ -21,7 +21,7 @@
 //#define TRACKER cleanTime
 //#define TRACKER calcLyap
 
-//#define TRACKANDPRINT 1
+#define TRACKANDPRINT 0
 
 #define WRITER writeNoabsRes
 //#define WRITER writeFixedRes
@@ -40,7 +40,7 @@
 
 #define HOLDTIME 20
 
-9 Dip
+//9 Dip
 #define NDIPS 10
 #define HEIGHTS {0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
 #define ENDTIMES {holdT,  holdT+40.0,  holdT+80.0,  holdT+100.0, holdT+120.0, holdT+140.0, holdT+160.0, holdT+180.0, holdT+200.0, holdT+300.0}
